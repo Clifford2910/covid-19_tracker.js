ad744b58692b9514131b615256d8453fa73a3daf
Alex <aclifford336@gmail.com>
2020-03-25 15:52:20 +0000

    added a components folder and a header component - plugged it into the app and 1 passing test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('can read the Header', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = getByText(/Covid-19 Global Tracker/i);
+  expect(header).toBeInTheDocument();
 });
