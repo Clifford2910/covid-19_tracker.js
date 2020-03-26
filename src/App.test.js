97305ad2c2592927c2eea5408a0970554c749450
Alex <aclifford336@gmail.com>
2020-03-26 16:48:17 +0000

    added tests for header2.js component

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('can read the Header', () => {
   const header = getByText(/Covid-19 Global Tracker/i);
   expect(header).toBeInTheDocument();
 });
+
+test('can read the second Header', () => {
+  const { getByText } = render(<App />);
+  const header2 = getByText(/Covid-19 Info by Country/i);
+  expect(header2).toBeInTheDocument();
+});
