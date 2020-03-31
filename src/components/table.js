4954deb136ef3a62da482e5fd14b6deb532f60ba
Alex <aclifford336@gmail.com>
2020-03-31 16:10:35 +0100

    added classnames to table to help with styling and started to test styling

--- src/components/table.js
+++ src/components/table.js
@@ -36,15 +36,15 @@ class SortableTable extends React.Component {
     let allCountryRows = []
     for (var i = 1; i < 183; i++) {
       allCountryRows.push(
-      <Table.Row key={this.props.cc[i].title}>
-        <Table.Cell>{this.props.cc[i].title}</Table.Cell>
-        <Table.Cell>{this.props.cc[i].total_cases}</Table.Cell>
-        <Table.Cell>{this.props.cc[i].total_recovered}</Table.Cell>
-        <Table.Cell>{this.props.cc[i].total_deaths}</Table.Cell>
-        <Table.Cell>{this.props.cc[i].total_active_cases}</Table.Cell>
-        <Table.Cell>{this.props.cc[i].total_serious_cases}</Table.Cell>
-        <Table.Cell>{this.props.cc[i].total_new_cases_today}</Table.Cell>
-        <Table.Cell>{this.props.cc[i].total_new_deaths_today}</Table.Cell>
+      <Table.Row className="bodyRow" key={this.props.cc[i].title}>
+        <Table.Cell className="bodyCell">{this.props.cc[i].title}</Table.Cell>
+        <Table.Cell className="bodyCell">{this.props.cc[i].total_cases}</Table.Cell>
+        <Table.Cell className="bodyCell">{this.props.cc[i].total_recovered}</Table.Cell>
+        <Table.Cell className="bodyCell">{this.props.cc[i].total_deaths}</Table.Cell>
+        <Table.Cell className="bodyCell">{this.props.cc[i].total_active_cases}</Table.Cell>
+        <Table.Cell className="bodyCell">{this.props.cc[i].total_serious_cases}</Table.Cell>
+        <Table.Cell className="bodyCell">{this.props.cc[i].total_new_cases_today}</Table.Cell>
+        <Table.Cell className="bodyCell">{this.props.cc[i].total_new_deaths_today}</Table.Cell>
       </Table.Row>
       )
     }
@@ -53,51 +53,59 @@ class SortableTable extends React.Component {
 
     return (
       <Table sortable celled fixed>
-        <Table.Header>
-          <Table.Row>
+        <Table.Header className="tableHeader">
+          <Table.Row className="headerRow">
             <Table.HeaderCell
+              className="headerCell"
               sorted={column === 'name' ? direction : null}
               onClick={this.handleSort('name')}
             >
               Name
             </Table.HeaderCell>
             <Table.HeaderCell
+              className="headerCell"
               sorted={column === 'Total Cases' ? direction : null}
               onClick={this.handleSort('Total Cases')}
             >
               Total Cases
             </Table.HeaderCell>
             <Table.HeaderCell
+              className="headerCell"
               sorted={column === 'Total Recovered' ? direction : null}
               onClick={this.handleSort('Total Recovered')}
             >
               Total Recovered
             </Table.HeaderCell>
             <Table.HeaderCell
+              className="headerCell"
               sorted={column === 'Total Deaths' ? direction : null}
               onClick={this.handleSort('Total Deaths')}
             >
               Total Deaths
             </Table.HeaderCell>
             <Table.HeaderCell
+              className="headerCell"
               sorted={column === 'Active Cases' ? direction : null}
               onClick={this.handleSort('Active Cases')}
             >
               Active Cases
             </Table.HeaderCell>
             <Table.HeaderCell
+              className="headerCell"
               sorted={column === 'Serious Cases' ? direction : null}
               onClick={this.handleSort('Serious Cases')}
             >
               Serious Cases
             </Table.HeaderCell>
             <Table.HeaderCell
+              className="headerCell"
               sorted={column === 'New Cases Today' ? direction : null}
               onClick={this.handleSort('New Cases Today')}
             >
               New Cases Today
             </Table.HeaderCell>
             <Table.HeaderCell
+              className="headerCell"
               sorted={column === 'New Deaths Today' ? direction : null}
               onClick={this.handleSort('New Deaths Today')}
             >
@@ -105,7 +113,7 @@ class SortableTable extends React.Component {
             </Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-        <Table.Body>
+        <Table.Body className="tableBody">
           {allCountryRows}
         </Table.Body>
       </Table>
